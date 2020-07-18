e4606a94e0f10ea1b955349b9e2132257ee89847
faelmendes <42940049+faelmendes@users.noreply.github.com>
2020-07-18 01:25:28 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -1,4 +1,3 @@
-#!/bin/bash/python2
 import socket
 from threading import Thread
 
@@ -19,16 +18,15 @@ class ServerThread(Thread):
         self.con.recv(1024)
         print palavra
         i=0
-        s=0
-        numero=0
-        numeros = "0123456789"
+        calculo=0
+        letras = [65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,
+                   97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122]
         while i < len(palavra):
-            for a in numeros:
-                if a == palavra[i]:
-                   s+= 1
+            for a in letras:
+                if chr(a) == palavra[i]:
+                   calculo+= 1
             i+=1
-        calculo = (i - s)
-        fim = "numero de letras e "+str(calculo)
+        fim = "numero de letras e "+str(calculo) #funciona com letras maiúsculas e menusculas
         self.con.send(fim)
         self.con.close()
 
@@ -45,19 +43,3 @@ while True:
     con, cliente = server.accept()
     print 'conectado com ',cliente
     conexao = ServerThread(con)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
